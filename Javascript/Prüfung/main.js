f580a0382c8d593275854de3d75b203c23129f08
Unknown <wilhelm.putz@teledialog.com>
2019-02-07 18:59:28 +0100

    üben

--- Javascript/Prüfung/main.js
+++ Javascript/Prüfung/main.js
@@ -1,48 +1,88 @@
+var merker = 0;
 
-
-$('#password, #secondpassword').keyup(function(){
-  var merker = 0;
-  var input = $(this).val();
+$('#password').keyup(function(){
+  merker=0;
+  var input = $(password).val();
   var vergleich = $(secondpassword).val();
+
   if( input.length >= 8 ){
     $('#length').addClass('valide');
     merker +=1;
-  } else {
-    $('#length').removeClass('valide');
-
   }
+  else {
+    $('#length').removeClass('valide');
+    merker -=1;
+  }
+
   if( input.match(/[A-z]/) ){
     $('#letter').addClass('valide');
     merker +=1;
-  } else {
+  }
+ else
+ {
     $('#letter').removeClass('valide');
+    merker -=1;
+  }
 
-  } if( input.match(/[A-Z]/) ){
+  if( input.match(/[A-Z]/) )
+  {
     $('#capital').addClass('valide');
     merker +=1;
-  } else {
+  }
+  else
+  {
     $('#capital').removeClass('valide');
+    merker -=1;
+  }
 
-  } if( input.match(/\d/) ){
+  if( input.match(/\d/) )
+  {
   $('#number').addClass('valide');
   merker +=1;
-  } else {
-    $('#number').removeClass('valide');
+  }
 
-  } if( input.match(/[!@#$%\^&*(){}[\]<>?/|\-+]/) ){
+  else
+  {
+    $('#number').removeClass('valide');
+    merker -=1;
+  }
+  if( input.match(/[!@#$%\^&*(){}[\]<>?/|\-+]/) )
+  {
   $('#special').addClass('valide');
    merker +=1;
-  } else {
+  }
+  else
+  {
     $('#special').removeClass('valide');
+    merker -=1;
+  }
 
-  }
-  if (merker == 5 && input == vergleich){
-    $('#passwordtext').addClass('valide');
-    merker +=1;
-    } else {
-      $('#passwordtext').removeClass('valide');
-  }
-  console.log(merker);
+  console.log('merker:' + merker);
   console.log(input);
   console.log(vergleich);
+});
+
+
+$('#password, #secondpassword').keyup(function(){
+
+  var input = $(password).val();
+  var vergleich = $(secondpassword).val();
+  if (input == vergleich && input!='' && merker == 5){
+    $('#passwordtext').addClass('valide');
+    $('#knopf').removeAttr('disabled');
+    }
+    else
+    {
+      $('#passwordtext').removeClass('valide');
+      $('#knopf').prop('disabled',true);
+      $('#knopf').attr("name", "Der Knopf");
+      $('#chk1,#chk2').prop("checked", true);
+      var x = $('#datepicker').val();
+      console.log(x);
+    }
+});
+
+$('#datepicker').on("change",function(){
+  var x = $('#datepicker').val();
+  console.log(x);
 });
